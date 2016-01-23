19aadf8c95d3816bc4efa59a4cbefcb7c3a1895c
Hideyasu <kutuzov1228@gmail.com>
2016-01-23 21:56:25 +0900

    formみかんせい

--- change-class-menu.js
+++ change-class-menu.js
@@ -1,46 +1,50 @@
 if (Meteor.isClient) {
-
-  	Template.serch_form.helpers({
+	var template  = Template.search_form;
+	template.onCreated(function () {
+		var chosenFloor = new ReactiveVar();
+		this.chosenFloor = chosenFloor;
+		this.autorun(function () {
+			chosenFloor.set(null)
+		})
+	})
+  	template.helpers({
   		floors: function(){
   			return [2,3,4,5,6,7,8,9];
-  		}
-  	});
-
-  	Template.serch_form.helpers({
+  		},
   		weeks: function(){
   			return ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-  		}
-  	});
-
-  	Template.serch_form.helpers({
+  		},
+  		isChosenFloor: function (floor) {
+  			return Template.instance().chosenFloor.get() === floor;
+  		},
+  		chosenFloor: function () {
+  			return Template.instance().chosenFloor.get();
+  		},
+  		rooms: function () {
+  			var chosenFloor = Template.instance().chosenFloor.get();
+  			if (chosenFloor == 2) {
+  				return ['A-1', 'A-2']
+  			}
+  			return []
+  		},
   		periods: function(){
   			return [1,2,3,4,5];
-  		}
-  	});
-
-  	Template.serch_form.helpers({
+  		},
   		lesson_names: function(){
   			return ["lesson1","lesson2","lesson3","lesson4","lesson5"];
-  		}
-  	});
-
-  	Template.serch_form.helpers({
+  		},
   		teachers: function(){
   			return ["teacher1","teacher2","teacher3","teacher4","teacher5"];
   		}
   	});
 
-  	Template.addsubject.events({
-    'change #floor' : function(event, template){
-      var floornum = parseInt(template.find('#floor').value);
-      template.find('#period').value = 4;
-      switch (floornum) {
-        case 4:
-          console.log("it works!!");
-        default:
-      }
-    }
-  });
+
+  	template.events({
+	    'change #floor': function(event, template) {
+	      	var floornum = parseInt(template.find('#floor').value);
+	      	Template.instance().chosenFloor.set(floornum)
+	    }
+  	});
 }
 
 
