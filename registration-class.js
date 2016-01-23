01c16448d7c65a410123123c9b2d5b50487a8617
Hideyasu <kutuzov1228@gmail.com>
2016-01-24 00:35:11 +0900

    とりあえず時間割登録画面完成

--- registration-class.js
+++ registration-class.js
@@ -1,10 +1,10 @@
 if (Meteor.isClient) {
-	var template  = Template.search_form;
+	var template  = Template.registration_form;
 	template.onCreated(function () {
 		var chosenFloor = new ReactiveVar();
 		this.chosenFloor = chosenFloor;
 		this.autorun(function () {
-			chosenFloor.set(null)
+			chosenFloor.set(null);
 		})
 	})
   	template.helpers({
@@ -22,27 +22,57 @@ if (Meteor.isClient) {
   		},
   		rooms: function () {
   			var chosenFloor = Template.instance().chosenFloor.get();
-  			if (chosenFloor == 2) {
-  				return ['A-1', 'A-2']
-  			}
-  			return []
+        switch(chosenFloor){
+          case 2:
+            return ['A-1', 'A-2'];
+            break;
+          case 3:
+            return ['A', 'B','C'];
+            break;
+          case 4:
+            return ['A', 'B','C','D'];
+            break;
+          case 5:
+            return ['A', 'B','C','D'];
+            break;
+          case 6:
+            return ['A', 'B','C','D'];
+            break;
+          case 7:
+            return ['A', 'B','C','D'];
+            break;
+          case 8:
+            return ['A', 'B','C','D'];
+            break;
+          case 9:
+            return ['D-1','D-2'];
+            break;
+          default:
+        }
+  			return [];
   		},
   		periods: function(){
   			return [1,2,3,4,5];
   		},
   		lesson_names: function(){
-  			return ["lesson1","lesson2","lesson3","lesson4","lesson5"];
+  			return ["lesson1","lesson2","lesson3","lesson4","lesson5","seminar"];
   		},
   		teachers: function(){
   			return ["teacher1","teacher2","teacher3","teacher4","teacher5"];
-  		}
+  		},
+      students:function(){
+        return ["class1","class2","class3","class4"];
+      },
+      departments:function(){
+        return ["SI","KS"];
+      }
   	});
 
 
   	template.events({
 	    'change #floor': function(event, template) {
 	      	var floornum = parseInt(template.find('#floor').value);
-	      	Template.instance().chosenFloor.set(floornum)
+	      	Template.instance().chosenFloor.set(floornum);
 	    }
   	});
 }
